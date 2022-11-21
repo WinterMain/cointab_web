4e9fc94f26d7682157bafe98a5a604e416ccff17
lidong(YLD/Winter) <aiyld@hotmail.com>
2022-11-21 16:56:49 +0800

    update

--- assets/js/waifu-tips.js
+++ assets/js/waifu-tips.js
@@ -185,7 +185,7 @@ function initModel(waifuPath){
 	
 	$.ajax({
         cache: true,
-        url: waifuPath+'waifu-tips.json?t=13',
+        url: waifuPath+'waifu-tips.json?t=1121',
         dataType: "json",
         success: function (result){
             $.each(result.mouseover, function (index, tips){
