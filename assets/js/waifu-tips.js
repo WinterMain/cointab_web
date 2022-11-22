a4510ceca5668c3087af2b323a62e3c07f2b6d24
lidong(YLD/Winter) <aiyld@hotmail.com>
2022-11-22 15:54:17 +0800

    update

--- assets/js/waifu-tips.js
+++ assets/js/waifu-tips.js
@@ -135,7 +135,7 @@ function showHitokoto(){
         // var text = '<span style="color:#0099cc;">{source}</span>，是来自<span style="color:#0099cc;">{from}</span>';
         // text = text.render({source: result.from, creator: result.creator});
         showMessage(result.hitokoto, 5000);
-        window.setTimeout(function() {showMessage(text, 3000);}, 5000);
+        // window.setTimeout(function() {showMessage(text, 3000);}, 5000);
     });
 	/*
 	$.getJSON('https://api.fghrsh.net/hitokoto/rand/?encode=jsc&uid=3335',function(result){
